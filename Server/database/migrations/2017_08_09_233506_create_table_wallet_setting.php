6f7c9004e8cecce1998057664b97e5b2c964961d
kaychen <kaychen.cn@gmail.com>
2017-08-09 23:43:10 +0800

    new table

---
+++ Server/database/migrations/2017_08_09_233506_create_table_wallet_setting.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableWalletSetting extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallet_setting', function (Blueprint $table) {
+            $table->increments('id');
+	    $table->string('wallet',100)->unique();
+	    $table->string('email',50);
+	    $table->string('balance',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallet_setting');
+    }
+}
